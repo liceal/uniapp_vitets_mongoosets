8ff656fb0f588fb8e5f5228f7c85a8a1d7a4bca4
linxianao <linxianao@jmcompany.cn>
2025-06-16 18:22:22 +0800

    feat: 增加下边条样式

--- unocss.config.ts
+++ unocss.config.ts
@@ -13,7 +13,9 @@ export default defineConfig({
     transformerDirectives(), // 启用指令功能
   ],
   // 自定义规则
-  // rules: [["m-1", { margin: "0.25rem" }]],
+  rules: [
+    ["border-bottom-ef", { "border-bottom": "1px solid #efefef" }], // 新增规则
+  ],
   // 快捷方式
   shortcuts: {
     btn: "py-2 px-4 rounded bg-blue-500 text-white",
