53e56a89af3e5a0cdd25374bd029914a6e15b6dc
linxianao <linxianao@jmcompany.cn>
2025-07-08 14:30:55 +0800

    fix: 修复小程序请求头无法自定义问题

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -18,18 +18,64 @@ baseURL = import.meta.env.VITE_GEN_PROXY_PATH;
 baseURL = import.meta.env.VITE_APP_BASE_API;
 // #endif
 
+const getResponse = (res: any, config: any) => {
+  const { statusCode, errMsg } = res;
+  const response = {
+    ...res,
+    status: statusCode,
+    statusText: errMsg,
+    config,
+    request: null,
+  };
+
+  return response;
+};
 const http = axios.create({
   baseURL,
   timeout: 10000,
-  adapter: UniAdapter, // 指定适配器
+  adapter: (config: InternalAxiosRequestConfig) => {
+    if (!uni) {
+      throw new Error("please use this in uni-app project!");
+    }
+    return new Promise((resolve, reject) => {
+      const { baseURL = "", url, headers, data, params } = config;
+      const uniConfig = {
+        ...config,
+        url: baseURL + url,
+        header: headers,
+      };
+
+      if (data || params) {
+        try {
+          uniConfig.data = JSON.parse(data || params);
+        } catch (e) {
+          uniConfig.data = data || params;
+        }
+      }
+      uni.request({
+        ...uniConfig,
+        header: {
+          ...uniConfig.headers, //不懂 这样小程序才能传递header
+        },
+        method: uniConfig.method as any,
+        success(res) {
+          const response = getResponse(res, config);
+          resolve(response);
+        },
+        fail(res) {
+          const response = getResponse(res, config);
+          reject(response);
+        },
+      });
+    });
+  },
 });
 
 // 请求拦截器
 http.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
+  (config: InternalAxiosRequestConfig & any) => {
     config.headers["Content-Type"] = "application/json; charset=utf-8";
     config.headers["authorization"] = uni.getStorageSync("token");
-
     // 可以在这里添加token等
     return config;
   },
